ef90a5f1dce233187e1c64aae374c0e04b81e35e
Safricloud <97082668+Safricloud@users.noreply.github.com>
2022-10-01 10:39:20 +0200

    .

--- local_modules/crypto.js
+++ local_modules/crypto.js
@@ -15,4 +15,63 @@ function generateHash(input) {
     return hash.digest('hex');
 };
 
+function encrypt(input, rounds) {
+    for (let i = 0; i < rounds; i++) {
+        input = encodeInput(input);
+    }
+    return input;
+}
+
+function decrypt(input, rounds) {
+    for (let i = 0; i < rounds; i++) {
+        input = decodeInput(input);
+    }
+    return input;
+}
+
+function encodeInput(input) {
+    // Char codes to be used 32 - 126, loop around if in excess of 126 or less than 32
+    let charCodes = [];
+    for (let i = input.length; i > 0; i--) {
+        let charCode = input.charCodeAt(i - 1);
+        //
+        // console.log('Before:')
+        // console.log(`Character: ${input[i - 1]} - Code: ${charCode}`);
+        charCode += i;
+        charCode = range(32, 126, charCode);
+        // console.log(`i = ${i}`);
+        // console.log('After:')
+        // console.log(`Character: ${String.fromCharCode(charCode)} - Code: ${charCode}`);
+        charCodes.push(charCode);
+    }
+    const response = String.fromCharCode(...charCodes);
+    return response;
+}
+
+function decodeInput(input) {
+    let charCodes = [];
+    for (let i = 0; i < input.length; i++) {
+        let charCode = input.charCodeAt(i);
+        charCode -= input.length - i;
+        charCode = range(32, 126, charCode);
+        charCodes.push(charCode);
+    }
+    const response = String.fromCharCode(...charCodes)
+        .split('')
+        .reverse()
+        .join('');
+    return response;
+}
+
+function range(start, end, value) {
+    if (value > end) {
+        value = start + (value - end + 1);
+    } else if (value < start) {
+        value = end - (start - 1 - value);
+    }
+    return value;
+}
+
 module.exports.generateHash = generateHash;
+module.exports.encrypt = encrypt;
+module.exports.decrypt = decrypt;
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,31 @@
-console.log('Script started...');
+"use strict";
 
 // HTML Objects
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const solvedCount = document.getElementById('solved-count');
+const totalCount = document.getElementById('total-count');
+const circle = document.getElementById('riddle-cipher');
 
 // Globals
 let currentInput = '';
 
 // Run on page load
 function init() {
-    console.log('Init...');
-    // Focus the input field
+    console.log('Initialising...');
     input.focus();
     input.addEventListener('keyup', (e) => {
         let entry = sanitize(e.target.value);
+        lighten();
         input.value = entry;
         if (currentInput !== entry) {
-            console.log('  -- Collecting API response...');
+            console.log('  -- Collecting API response -- ');
             currentInput = entry;
             inputListener(entry);
-        } else {
         }
     });
+    loop();
+    console.log('Ready.');
 }
 
 function sanitize(input) {
@@ -45,15 +49,52 @@ function sanitize(input) {
     }
 }
 
+function lighten() {
+    let start = circle.style.backgroundColor;
+    if (typeof start === 'string' && start.length > 0) {
+        let opacity = start.split(',')[3].substring(0, start.split(',')[3].length - 1);
+        opacity = parseFloat(opacity);
+        if (opacity < 1) {
+            opacity += 0.1;
+            if (opacity >= 1) {
+                opacity = 0.99;
+            }
+        } else {
+            opacity = 0.99;
+        }
+        circle.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+    } else {
+        circle.style.backgroundColor = `rgba(255, 255, 255, 0.1)`;
+    }
+}
+
+function darken(fps) {
+    let increment = fps / 100 * 0.01;
+    let start = circle.style.backgroundColor;
+    if (typeof start === 'string' && start.length > 0) {
+        let opacityraw = start.split(',')[3]
+        let opacity = opacityraw.substring(0, opacityraw.length - 1);
+        opacity = parseFloat(opacity);
+        if (opacity > 0) {
+            opacity -= increment;
+            circle.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+        }
+    } else {
+        circle.style.backgroundColor = `rgba(255, 255, 255, 0)`;
+    }
+}
+
 function inputListener(input) {
     fetchCipher(input)
         .then((response) => response.json())
         .then((data) => {
             console.log(data.output);
             if (data.complete) {
-                animate();
+                complete();
             }
             output.textContent = data.output;
+            solvedCount.textContent = data.solvedCount;
+            totalCount.textContent = data.totalCount;
         })
         .catch((error) => {
             console.error('Error:', error);
@@ -72,8 +113,36 @@ function fetchCipher(input) {
     });
 }
 
-function animate(count) {
+function complete() {
     document.getElementById('overlay').style.display = 'flex';
+}
+
+function loop() {
+    let then = Date.now();
+    let now;
+    let elapsed;
+    let frames = 0;
+    let fps = 0;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        frames++;
+        now = Date.now();
+        elapsed = now - then;
+        then = now;
+        darken(fps);
+    }
+
+    function initfps() {
+        setInterval(() => {
+            //console.log('FPS: ', frames);
+            fps = frames;
+            frames = 0;
+        }, 1000);        
+    }
+
+    initfps();
+    animate();
 }
 
 window.onload = init;
